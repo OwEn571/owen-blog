22fd17dd771b0cf3dfaeb87855ba7ef587234bfb
OwEn571 <owen@OwEndeMacBook-Air.local>
2026-03-30 10:13:46 +0800

    更新

--- src/content/posts/study/算法题/test.py
+++ src/content/posts/study/算法题/test.py
@@ -1,40 +1,24 @@
-def solution(s: str, p: str) -> list[int]:
-    ans = []
-    need = {}
-    window = {}
-    left, right = 0, 0
-    valid = 0
+import ast
 
-    for c in p:
-        need[c] = need.get(c, 0) + 1
-
-    while right < len(s):
-        c = s[right]
+def solution(nums:list,k:int) -> int:
+    left,right = 0,0
+    count = 0
+    total = 0
+    while right<len(nums):
+        c = nums[right]
         right += 1
-        # 只统计需要的
-        if c in need:
-            window[c] = window.get(c, 0) + 1
-            if window[c] == need[c]:
-                valid += 1
-
-        # 固定窗口长度，超过 len(p) 就收缩
-        while right - left > len(p):
-            d = s[left]
+        total += c
+        while total > k:
+            d = nums[left]
             left += 1
-
-            if d in need:
-                if window[d] == need[d]:
-                    valid -= 1
-                window[d] -= 1
-
-        # 长度刚好且所有字符频次都匹配，记录答案
-        if right - left == len(p) and valid == len(need):
-            ans.append(left)
-
-    return ans
+            total -= d
+        if total == k:
+            count += 1
+    return count
+    
 
 
 if __name__ == "__main__":
-    s = input().strip()
-    p = input().strip()
-    print(solution(s, p))
+    nums = ast.literal_eval(input().strip())
+    k = int(input().strip())
+    print(solution(nums,k))
